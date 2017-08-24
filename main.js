6442e618f2ad1855cbbc41d533ee2b074823735a
Noah Lyons <noahlyons@Noahs-MacBook-Pro.local>
2017-08-23 18:14:28 -0700

    finished and cleaned up

--- main.js
+++ main.js
@@ -1,40 +1,53 @@
 var camera, scene, renderer;
 var mesh;
-
+var vertPairs;
+var counter = 0;
+var verts;
+var segments = 100;
+var height = 50;
+var thickness = 2;
+var offset = 10;
 init();
 animate();
 
 function init() {
 
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000 );
-	camera.position.z = 10;
+	// camera.position.z = 30;
+	// camera.position.y = 10;
+	// camera.position.x = 30;
+	// // camera.up = new THREE.Vector3(0,1,0);
+	// camera.lookAt(new THREE.Vector3(0,5,0));
+
+	camera.position.z = 30;
+	camera.position.y = 30;
+	camera.position.x = 0;
+	// camera.up = new THREE.Vector3(0,1,0);
+	camera.lookAt(new THREE.Vector3(0,15,0));
 
 	scene = new THREE.Scene();
-
-	// var texture = new THREE.TextureLoader().load( 'textures/crate.gif' );
-
-	// var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
 
 	var planeGeo = new THREE.Geometry();
 	planeGeo.dynamic = true;
 
-	// var vertices = []
-	// for(var i=0;i<10;i+= 1) {
-	// 	for(var j=0;j<2;j++){
-	// 		vert = new THREE.Vector3(i*10, j*10, 0);
-	// 		vertices.push(vert);
-	// 	}
-	// 	vert = new THREE.Vector3((i*10)+10, j*10, 0);
-	// 	vertices.push(vert);
-	// }
-
-	verts = []
-	for (var i = 0; i < 10; i++){
-		verts.push(
-			new THREE.Vector3(i + 0,0,0),
-			new THREE.Vector3(i + 1,0,0),
-			new THREE.Vector3(i + 0,1,0)
-		)
+	verts = [];
+	vertPairs = [];
+	var index = 0
+	// LEFT SIDE
+	for(var i = 0; i < segments; i++)
+	{
+		var vert = new THREE.Vector3(0, ((height * i) + offset) / segments, 10);
+		verts.push(vert);
+		vertPairs[i] = [index];
+		index++;
+	}
+	// RIGHT SIDE
+	for(var i = segments - 1; i >= 0; i--)
+	{
+		var vert = new THREE.Vector3(20, ((height * i) + offset) / segments, 10);
+		verts.push(vert);
+		vertPairs[i].push(index)
+		index++;
 	}
 
 	planeGeo.vertices = verts;
@@ -47,21 +60,31 @@ function init() {
 		planeGeo.faces.push( new THREE.Face3( triangles[i][0], triangles[i][1], triangles[i][2]));
 	}
 
-	var material = new THREE.MeshBasicMaterial();
+	planeGeo.computeFaceNormals();
+	planeGeo.computeVertexNormals();
 
+	var material = new THREE.MeshPhongMaterial({
+		color: new THREE.Color().setRGB( 0.8, 0.70, 0.9 ),
+	});
+
+	var light = new THREE.PointLight(0x0040ff)
+	light.position.x = 40;
+	light.position.y = 20;
+	light.position.z = 15;
+	light.intensity = 10.5;
+	scene.add(light);
 
 	mesh = new THREE.Mesh( planeGeo, material );
+	mesh.dynamic = true;
 	scene.add( mesh );
+	mesh.rotation.z = 1.55;
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setPixelRatio( window.devicePixelRatio );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 
-	//
-
 	window.addEventListener( 'resize', onWindowResize, false );
-
 }
 
 function onWindowResize() {
@@ -76,10 +99,13 @@ function onWindowResize() {
 function animate() {
 
 	requestAnimationFrame( animate );
-
-	mesh.rotation.x += 0.005;
-	mesh.rotation.y += 0.01;
-
+	counter += 0.1;
+	for (var i = 0; i < segments; i++)
+	{
+		mesh.geometry.vertices[vertPairs[i][0]].x += 0.2 * Math.sin(counter + 16 * mesh.geometry.vertices[vertPairs[i][0]].y / height);
+		mesh.geometry.vertices[vertPairs[i][1]].x += 0.2 * Math.sin(counter + 16 * mesh.geometry.vertices[vertPairs[i][1]].y / height);
+	}
+	mesh.geometry.verticesNeedUpdate = true;
 	renderer.render( scene, camera );
 
 }
